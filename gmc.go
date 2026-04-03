61a65b4617e20c86c64c0a807064af4923651b39
Felix Reiter <sbody1113@gmail.com>
2026-04-03 20:25:18 +0200

    removed safeIndex

--- gmc.go
+++ gmc.go
@@ -1,25 +1,25 @@
 package main //TODO: Find a good name!
 import (
 	"bytes"
-	"errors"
-	"strconv"
-
-	// "fmt"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 var OPTION_AUTOCLEAR_ARG = false
-func safeIndex(slice []any, index int) (any, error) {
+/*
+func safeIndex[T any | int](slice []T, index int) (T, error) {
 	if index < 0 {
 		log.Fatal("PANIC: SAFEINDEX PARAM < 0 <GMC>")
 	}
-	if len(slice) < index {
+	if len(slice) < index + 1 {
 		return 0, errors.New("error: not safe indexing")
 	} else {
 		return slice[index], nil
 	}
 }
+	*/
 func intToBin(int_ int) string {
 	return strconv.FormatInt(int64(int_), 2)
 }
@@ -39,26 +39,39 @@ func GetInstructions(path string) [][]byte {
 	return cleaned
 }
 func ReadInstruction(instruction []byte) {
+	fmt.Println(instruction)
 	itype := int(instruction[0])
 	if itype < 0xA9 {
 		log.Fatal("PANIC: INVALID INSTRUCTION <GMC>")
 	}
-
+	fmt.Println(itype)
 	switch itype {
 	case 0xA9:
 		os.Exit(0)
 	case 0xAA:
-		optionsAny, err := safeIndex([]any{instruction}, 1)
-		if err != nil {
+		if len(instruction) < 2 {
 			log.Fatal("PANIC: INVALID INSTRUCTION [OPTION <MISSING>] <GMC>")
 		}
-		binoptions := intToBin(optionsAny.(int))
+		options := instruction[1] 
+		
+		binoptions := intToBin(int(options))
 		if binoptions[0] == '1' {
 			OPTION_AUTOCLEAR_ARG = true
 		} //todo: add more options
+	case 0xAB:
+		if len(instruction) < 2 {
+			log.Fatal("PANIC: INVALID INSTRUCTION [PUSHSTR <MISSING> ...] <GMC>")
+		}
+		stackany := instruction[1]
+		
+		stack := int(stackany)
+		if stack > 2 {
+			log.Fatal("PANIC: INVALID INSTRUCTION [PUSHSTR <TO_BIG> ...] <GMC>")
+		}
+		//TODO: Continue
 	}
 }
 
 func main() {
-	//tests go here
+	ReadInstruction(GetInstructions("instructions.gmc")[0])
 }
