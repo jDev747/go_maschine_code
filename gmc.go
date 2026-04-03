2da5322ccbbf77a1f1764f7d56ce0152158eb234
Felix Reiter <sbody1113@gmail.com>
2026-04-03 19:40:25 +0200

    Got Instruction Splitting working!

--- gmc.go
+++ gmc.go
@@ -1,8 +1,29 @@
 package main //TODO: Find a good name!
-
-
-
+import (
+	"bytes"
+	// "fmt"
+	"log"
+	"os"
+)
+func GetInstructions(path string) [][]byte {
+	bytesoriginal, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bytesnew := bytes.Split(bytesoriginal, []byte{0xA8})
+	cleaned := make([][]byte, 0, len(bytesnew))
+	for _, bytearr := range bytesnew {
+		
+		if len(bytearr) > 0 {
+			cleaned = append(cleaned, bytearr)
+		}
+	}
+	return cleaned
+}
+func ReadInstruction(intruction []byte) {
+	// TODO: make this
+}
 
 func main() {
-	//Tests go here
+	//tests go here
 }
