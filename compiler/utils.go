54261f3732bed3b3fb78589d1ec796131c42d179
Felix Reiter <sbody1113@gmail.com>
2026-04-05 10:39:44 +0200

    Changed algorithm

--- compiler/utils.go
+++ compiler/utils.go
@@ -22,14 +22,19 @@ func StrToInt(str string, base int) int64 {
 		}
 	return i
 }
+// the old one was VERY bad
 func IntToBytes(n int) []byte {
     if n == 0 {
         return []byte{0}
     }
-    var bytes []byte
-    for n > 0 {
-        bytes = append([]byte{byte(n & 0xFF)}, bytes...)
-        n >>= 8
+    s := strconv.Itoa(n)
+    if len(s)%2 == 1 {
+        s = "0" + s
     }
-    return bytes
-} // this is a standart algorithm for doing stuff like this apparently
+    result := make([]byte, len(s)/2)
+    for i := 0; i < len(s); i += 2 {
+        pair, _ := strconv.Atoi(s[i : i+2])
+        result[i/2] = byte(pair)
+    }
+    return result
+}
